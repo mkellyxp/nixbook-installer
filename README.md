71fe96fc7cf72e9ab6190f25cf357cfe33c5becc
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-11-11 17:18:42 -0800

    Add options to build standard or bundled iso

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
-## Instructions for creating the image
+# Nixbook Live Session and Installer ISO Builder
+This installer is very similar to the official NixOS installer which uses the Calamares installer with the GNOME or Plasma desktop environment, with the following features:
+- Has the same desktop environment (Cinnamon) and preinstalled apps as Nixbook
+- Automatically installs Nixbook instead of a generic NixOS installation
+- Gives you the option between installing Nixbook and Nixbook Lite
 
+## Requirements
 *have flakes and nix-command enabled*
 
 1. Clone this repo
 2. cd into the folder
-3. Run ```nix build .#packages.x86_64-linux.install-iso```
-4. The ISO file will be in ```/result/iso/```
+
+## Quickly testing the installer in a VM
+This method is much faster than creating an ISO because it can boot the VM without creating an ISO.
+```bash
+nix run .#packages.x86_64-linux.vm
+```
+
+## Instructions for creating the image
+There are two versions of the ISO, `standard` and `bundled`. 
+
+### `standard`
+This is similar to a NixOS installer. It has a live graphical session, but still downloads a lot of packages from the internet during installation. The output ISO size is ~3.7GB. This is ideal if you will only be installing Nixbook up to 3 times.
+
+Run 
+```bash
+nix build .#packages.x86_64-linux.standardIso
+```
+The ISO file will be in `/result/iso/`.
+
+### `bundled`
+This includes everything in `standard`, plus it has most required Nix packages bundled in the ISO so that only a small amount (~150 MB) of data will need to be downloaded from the internet when installing Nixbook. The output ISO size is ~28GB. This is ideal if you will be installing Nixbook >3 times.
+
+Run 
+```bash
+nix build .#packages.x86_64-linux.bundledIso
+```
+The ISO file will be in `/result/iso/`.
--- flake.nix
+++ flake.nix
@@ -18,12 +18,14 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          config.allowUnfree = true;
-          config.allowInsecurePredicate =
-            pkg:
-            builtins.elem (nixpkgs.lib.getName pkg) [
-              "broadcom-sta" # aka “wl”
-            ];
+          config = {
+            allowUnfree = true;
+            allowInsecurePredicate =
+              pkg:
+              builtins.elem (nixpkgs.lib.getName pkg) [
+                "broadcom-sta" # aka “wl”
+              ];
+          };
           overlays = [
             (final: prev: {
               calamares-nixos-extensions = prev.calamares-nixos-extensions.overrideAttrs (old: {
@@ -36,70 +38,79 @@
             })
           ];
         };
-
-        nixosConfiguration = nixpkgs.lib.nixosSystem {
-          inherit pkgs system;
-          modules = [
+        liveInstallerModule = {
+          imports = [
             "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares.nix"
             ./common.nix
+          ];
+          nix.extraOptions = "experimental-features = nix-command flakes";
+          isoImage.isoName = "nixos-custom-installer.iso";
+          system.stateVersion = "25.05";
+
+          # Adapted from installation-cd-graphical-calamares-gnome.nix
+          isoImage.edition = "nixbook";
+          services.xserver.desktopManager.cinnamon.extraGSettingsOverrides = ''
+            [org.cinnamon.desktop.session]
+            idle-delay=0
+
+            [org.cinnamon.settings-daemon.plugins.power]
+            sleep-inactive-ac-type='nothing'
+            sleep-inactive-battery-type='nothing'
+          '';
+
+          services.displayManager.autoLogin = {
+            enable = true;
+            user = "nixos";
+          };
+
+          # After installation, these packages will be installed through Flatpak and not NixOS
+          # But they are here to demo how Nixbook would be like after installing
+          environment.systemPackages = with pkgs; [
+            google-chrome
+            zoom
+            libreoffice
+          ];
+          # Copy Nixbook home files
+          system.userActivationScripts = {
+            copyNixbookHomeFiles = {
+              text =
+                let
+                  config = ./config;
+                  guides = ./guides;
+                in
+                ''
+                  # Without the install 0644, the .config and Desktop dirs are read-only
+                  # I think this is because they get copied form the nix store, which is read-only
+                  cd ${config} && find . -type f -exec install -Dm 0644 "{}" "$HOME/.config/{}" \;
+                  mkdir -p $HOME/Desktop
+                  for f in "${guides}"/*; do
+                    ln -s "$f" "$HOME/Desktop/$(basename "$f")"
+                  done
+                '';
+            };
+          };
+
+          # Options that get used by system.build.vm
+          virtualisation.vmVariant.virtualisation = {
+            # 4 cores are recommended for Nixbook
+            cores = 4;
+            # 4GB memory is recommended for Nixbook
+            memorySize = 4096;
+            # Don't persist any data
+            diskImage = null;
+          };
+        };
+        standardConfiguration = nixpkgs.lib.nixosSystem {
+          inherit pkgs system;
+          modules = [
+            liveInstallerModule
+          ];
+        };
+        bundledConfiguration = nixpkgs.lib.nixosSystem {
+          inherit pkgs system;
+          modules = [
+            liveInstallerModule
             {
-              nix.extraOptions = "experimental-features = nix-command flakes";
-              isoImage.isoName = "nixos-custom-installer.iso";
-              system.stateVersion = "25.05";
-
-              # Adapted from installation-cd-graphical-calamares-gnome.nix
-              isoImage.edition = "nixbook";
-              services.xserver.desktopManager.cinnamon.extraGSettingsOverrides = ''
-                [org.cinnamon.desktop.session]
-                idle-delay=0
-
-                [org.cinnamon.settings-daemon.plugins.power]
-                sleep-inactive-ac-type='nothing'
-                sleep-inactive-battery-type='nothing'
-              '';
-
-              services.displayManager.autoLogin = {
-                enable = true;
-                user = "nixos";
-              };
-
-              # After installation, these packages will be installed through Flatpak and not NixOS
-              # But they are here to demo how Nixbook would be like after installing
-              environment.systemPackages = with pkgs; [
-                google-chrome
-                zoom
-                libreoffice
-              ];
-              # Copy Nixbook home files
-              system.userActivationScripts = {
-                copyNixbookHomeFiles = {
-                  text =
-                    let
-                      config = ./config;
-                      guides = ./guides;
-                    in
-                    ''
-                      # Without the install 0644, the .config and Desktop dirs are read-only
-                      # I think this is because they get copied form the nix store, which is read-only
-                      cd ${config} && find . -type f -exec install -Dm 0644 "{}" "$HOME/.config/{}" \;
-                      mkdir -p $HOME/Desktop
-                      for f in "${guides}"/*; do
-                        ln -s "$f" "$HOME/Desktop/$(basename "$f")"
-                      done
-                    '';
-                };
-              };
-
-              # Options that get used by system.build.vm
-              virtualisation.vmVariant.virtualisation = {
-                # 4 cores are recommended for Nixbook
-                cores = 4;
-                # 4GB memory is recommended for Nixbook
-                memorySize = 4096;
-                # Don't persist any data
-                diskImage = null;
-              };
-
               # Bundle in all possible packages we might use
               # From https://github.com/pete3n/nix-offline-iso/blob/6dd16a6e9634d61788096d3b7f2c2ca07af40a2f/flake.nix#L53-L55
               isoImage.storeContents = [
@@ -113,8 +124,11 @@
         };
       in
       {
-        packages.install-iso = nixosConfiguration.config.system.build.isoImage;
-        packages.vm = nixosConfiguration.config.system.build.vm;
+        packages = {
+          vm = standardConfiguration.config.system.build.vm;
+          standardIso = standardConfiguration.config.system.build.isoImage;
+          bundledIso = bundledConfiguration.config.system.build.isoImage;
+        };
       }
     );
 }
